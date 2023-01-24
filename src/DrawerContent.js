2d252a557a307b69a3c5a7d15932d4160eaea601
Prasanna <gramopadhye37@gmail.com>
2023-01-24 18:15:11 +0530

    First commit

---
+++ src/DrawerContent.js
@@ -0,0 +1,51 @@
+import { View } from 'react-native'
+import React from 'react'
+import {
+    DrawerContentScrollView,
+    DrawerItem
+} from "@react-navigation/drawer"
+
+import {
+    Avatar,
+    Title,
+    Caption,
+    Paragraph,
+    Drawer,
+    Text,
+    TouchableRipple,
+    Switch
+} from "react-native-paper"
+import { useNavigation } from '@react-navigation/native'
+
+const DrawerContent = ({ props }) => {
+    const navigation = useNavigation()
+    return (
+        <View style={{ flex: 1 }}>
+            <DrawerContentScrollView {...props}>
+                <View style={[styles.userInfoSection, { flexDirection: 'row' }]}>
+                    <View style={{ marginTop: 15 }}>
+                        <Avatar.Image source={require("./assets/images/prasanna.jpeg")} />
+                    </View>
+                    <View style={{ marginLeft: 9, marginTop: 15 }}>
+                        <Title style={styles.title}>Prasanna</Title>
+                        <Caption style={styles.caption}>Pune, India</Caption>
+                    </View>
+                </View>
+                <Drawer.Section style={{ flex: 1, marginTop: 15 }}>
+                    <DrawerItem label="Home" onPress={() => { navigation.navigate('Home') }} />
+                    <DrawerItem label="Account Details" onPress={() => { navigation.navigate('Account Details') }} />
+                    <DrawerItem label="My Orders" onPress={() => { navigation.navigate('My Orders') }} />
+                </Drawer.Section>
+            </DrawerContentScrollView>
+        </View>
+
+    )
+}
+
+export default DrawerContent
+
+const styles = {
+    userInfoSection: {},
+    title: {},
+    caption: {}
+}
---
+++ src/screens/AccountDetails.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AccountDetails = () => {
+  return (
+    <View>
+      <Text>AccountDetails</Text>
+    </View>
+  )
+}
+
+export default AccountDetails
